255c297c85b58824bd6c8e005fef1bd4e8db0df2
Blacksheep1995 <13918539500@163.com>
2022-04-25 21:18:51 +0800

    代码块中增加按钮可以复制值

--- docs/.vitepress/components/codebox.js
+++ docs/.vitepress/components/codebox.js
@@ -1,33 +1,52 @@
-//codebox.js是一个方法用来在所有的代码块里增加一个copy按钮,点击这个按钮可以复制代码块里的内容到剪贴板
 const codebox = () => {
-  //选择所有的div下面的pre元素放到一个数组中,这个数组就是currentDiv
+  //找到所有div下面的pre元素,放到数组currentDiv中
   let currentDiv = document.querySelectorAll("div pre")
   currentDiv.forEach((item) => {
-    //创建一个按钮
+    //创建一个新按钮
     let newButton = document.createElement("button");
     // 给它一些内容
     let newContent = document.createTextNode("copy");
     // 添加文本节点 到这个新的 button 元素
     newButton.appendChild(newContent);
     
-    //找到当前这个pre元素的父节点parent
+    //找到这个pre元素的父节点
     let parent = item.parentNode
-    
-    //在这个pre元素前插入刚刚创建的button按钮
+  
     parent.insertBefore(newButton,item);
 
     let code_content = item.querySelector("code")
+    // document.execCommand方法已经被弃用了,最好不要用
+    newButton.addEventListener('click',copy)
+    function copy(){
+      let input = document.createElement('input')
+      input.value = code_content.textContent
+      document.body.appendChild(input);
+      input.select()
+      document.execCommand("copy")
+      document.body.removeChild(input)
+      console.log('Page URL copied to clipboard');
+  }
 
-
-    newButton.addEventListener('click',async (e) => {
-      try {
-        await navigator.clipboard.writeText(code_content.textContent);
-        console.log('Page URL copied to clipboard');
-      } catch (err) {
-        console.error('Failed to copy: ', err);
-      }
-    })
+    // newButton.addEventListener('click',async (e) => {
+    //   try {
+    //     await navigator.clipboard.writeText(code_content.textContent);
+    //     console.log('Page URL copied to clipboard');
+    //   } catch (err) {
+    //     console.error('Failed to copy: ', err);
+    //   }
+    // })
   })
+  // document.execCommand方法已经被弃用了,最好不要用
+  // newButton.addEventListener('click',copy)
+  // function copy(){
+  //   let input = document.createElement('input')
+  //   input.value = code_content.textContent
+  //   document.body.appendChild(input);
+  //   input.select()
+  //   document.execCommand("copy")
+  //   document.body.removeChild(input)
+  //   alert("复制成功")
+  // } 
 }
 
 export default codebox
